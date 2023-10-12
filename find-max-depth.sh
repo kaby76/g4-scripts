4b6514e446143246138365ac63dc6365cc8833a5
Ken Domino <kenneth.domino@domemtech.com>
2023-10-12 07:13:29 -0400

    Add scripts to compute max depth of parse tree, and find nodes/text in parse tree that has max depth.

---
+++ find-max-depth.sh
@@ -0,0 +1,51 @@
+#
+
+while getopts 'xh' opt; do
+    case "$opt" in
+    x)
+        set -x
+        ;;
+    ?|h)
+        cat - <<EOF
+NAME
+       $(basename $0) - get the depth of the deepest node in the parse tree
+
+SYNOPSIS
+       $(basename $0) ([-x | -h])* [files]
+
+DESCRIPTION
+       This script must be run under Linux Bash or Windows MSYS2 Bash or Windows WSL Linux.
+
+OPTIONS
+    -h
+        Output this help message.
+    -x
+        Execute "set -x" to debug script.
+
+EXAMPLE USAGE
+
+EOF
+        exit 0
+        ;;
+    esac
+done
+shift $((OPTIND - 1))
+files=("$@")
+temp=`mktemp`
+if [ ${#files[@]} -gt 0 ]
+then
+    trparse -l ${files[@]} > $temp
+else
+    cat - > $temp
+fi
+
+cat $temp | trxgrep ' //*[not(*)
+  and
+   count(ancestor::*)
+  =
+   max(//*[not(*)]/count(ancestor::*))
+   ]
+     /(self::node|..)
+' | trcaret
+
+rm -f $temp
---
+++ max-depth.sh
@@ -0,0 +1,44 @@
+#
+
+while getopts 'xh' opt; do
+    case "$opt" in
+    x)
+        set -x
+        ;;
+    ?|h)
+        cat - <<EOF
+NAME
+       $(basename $0) - get the depth of the deepest node in the parse tree
+
+SYNOPSIS
+       $(basename $0) ([-x | -h])* [files]
+
+DESCRIPTION
+       This script must be run under Linux Bash or Windows MSYS2 Bash or Windows WSL Linux.
+
+OPTIONS
+    -h
+        Output this help message.
+    -x
+        Execute "set -x" to debug script.
+
+EXAMPLE USAGE
+
+EOF
+        exit 0
+        ;;
+    esac
+done
+shift $((OPTIND - 1))
+files=("$@")
+temp=`mktemp`
+if [ ${#files[@]} -gt 0 ]
+then
+    trparse ${files[@]} > $temp
+else
+    cat - > $temp
+fi
+
+cat $temp | trxgrep ' max(//*[not(*)]/count(ancestor::*))'
+
+rm -f $temp
