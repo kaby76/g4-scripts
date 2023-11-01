3c1ee52d4d74e10e3a3eed649939f7033dfbe61e
Ken Domino <kenneth.domino@domemtech.com>
2023-11-01 11:47:11 -0400

    Remove double-quotes around xpath exprs.

--- bison.sh
+++ bison.sh
@@ -2,13 +2,12 @@
 
 if [[ $# -gt 0 ]]
 then
-	echo Arguments were provided.
-	echo Converting bison grammar.
-	trparse -l $@ 2> /dev/null | \
-		trquery -i '
-			delete "//input_/prologue_declarations";
-			delete "//input_/PercentPercent";
-			delete "//input_/epilogue_opt";
-			delete "//BRACED_CODE";' | \
-		trtext
+	trparse $@ 2> /dev/null | \
+		trquery '
+			delete //input_/prologue_declarations;
+			delete //input_/PercentPercent;
+			delete //input_/epilogue_opt;
+			delete //BRACED_CODE;
+			delete //actionBlock;
+			delete //epilogue_opt;'
 fi
