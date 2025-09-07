fb62a6e9cb73ee37392712640182903fe5329c3e
Ken Domino <kenneth.domino@domemtech.com>
2025-09-07 04:07:36 -0400

    Updates

--- wip/find-low-k.sh
+++ wip/find-low-k.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dotnet trperf -c kF $@ 2> /dev/null \
+dotnet trperf -c mF $@ 2> /dev/null \
 	| grep -v '^0' \
 	| awk '{sum[$2] += $1} END {for (key in sum) print sum[key], key}' \
 	| sort -k1 -n
